ca091c6cb4dbe6da594f003367d15628153a1003
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-10-03 18:56:26 -0300

    depoimentos fim

--- src/components/Depoimento/Depoimento.jsx
+++ src/components/Depoimento/Depoimento.jsx
@@ -3,7 +3,7 @@ import "./Depoimento.css"
 export function Depoimento(props) {
     return (
         <div className="depoimento">
-            <img src={props.img} class="" alt="" />
+            <img src={props.img} class="img" alt="" />
             <div className="stars">
                 <i class="fa-solid fa-star estrela"></i>
                 <i class="fa-solid fa-star estrela"></i>
@@ -11,7 +11,10 @@ export function Depoimento(props) {
                 <i class="fa-solid fa-star estrela"></i>
                 <i class="fa-solid fa-star estrela"></i>
             </div>
-            <p className="comentario">{props.msg}</p>
+            <div className="texto">
+                <p className="comentario">{props.msg}</p>
+                <p className="comentario">{props.autor}</p>
+            </div>
         </div>
     )
 }
--- src/components/Depoimentos/Depoimentos.jsx
+++ src/components/Depoimentos/Depoimentos.jsx
@@ -8,23 +8,23 @@ export function Depoimentos() {
         <div className="depoimentos">
             <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
-                    <div class="carousel-item active" data-bs-interval="3000">
-                        <Depoimento img={img_cr7} msg="'Esse app é o melhor que tem. Amei usar ele e recomendo pra todos!' -Cristiano Ronaldo" />
+                    <div class="carousel-item active" data-bs-interval="5000">
+                        <Depoimento img={img_cr7} msg='"Comida excepcional! Pedi um prato com ingredientes frescos e sofisticados, e tudo veio no ponto perfeito. A apresentação estava digna de um restaurante de alta gastronomia."' autor=" -Cristiano Ronaldo" />
                     </div>
-                    <div class="carousel-item" data-bs-interval="3000">
-                        <Depoimento img={img_messi} msg="'Estou usando esse app ah 1 mês e amei!' -Lionel Messi" />
+                    <div class="carousel-item" data-bs-interval="5000">
+                        <Depoimento img={img_messi} msg='"O atendimento foi impecável, desde o pedido até a entrega. A comida chegou em embalagens elegantes e em temperatura ideal. Excelente para quem aprecia o luxo em cada detalhe."' autor="-Lionel Messi" />
                     </div>
-                    <div class="carousel-item" data-bs-interval="3000">
-                        <Depoimento img={img_neymar} msg="'Que app incrível!' -Neymar jr" />
+                    <div class="carousel-item" data-bs-interval="5000">
+                        <Depoimento img={img_neymar} msg='"Fiquei impressionado com a qualidade dos ingredientes. Pedi um filé mignon com molho especial e foi uma das melhores refeições que já tive, comparável aos melhores restaurantes."'  autor="-Neymar Jr" />
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
+                    <span class="visually-hidden">Antes</span>
                 </button>
                 <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
+                    <span class="visually-hidden">Próxima</span>
                 </button>
             </div>
 
