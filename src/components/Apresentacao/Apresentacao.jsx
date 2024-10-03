9049ad8d6f929d2b92932f9932d90c88efcf7be7
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-10-03 20:39:30 -0300

    final_apresentacao

--- src/components/Apresentacao/Apresentacao.jsx
+++ src/components/Apresentacao/Apresentacao.jsx
@@ -1,18 +1,18 @@
-import { ApresentacaoCard } from "../ApresentacaoCard/ApresentacaoCard";
+import { ApresentacaoCard } from "../ApresentacaoCard/ApresentacaoCard.jsx";
 
 export function Apresentacao() {
     return (
         <div className="Apresentacao">
             <section className="twocard">
-                <ApresentacaoCard icon="fa-solid fa-utensils" msg="Saboreie pratos incrivelmente preparados e feitos com ingredientes frescos, garantindo uma experiência culinária única em cada pedido."/>
+                <ApresentacaoCard icon="fa-solid fa-utensils" msg="Aproveite pratos deliciosos e frescos, com ingredientes de alta qualidade para uma experiência única."/>
 
-                <ApresentacaoCard icon="fa-solid fa-store" msg="Descubra os melhores restaurantes e opções gastronômicas próximas a você, além de explorar novos sabores e pratos especiais do seu gosto."/>
+                <ApresentacaoCard icon="fa-solid fa-store" msg="Explore restaurantes próximos e descubra novos sabores que combinam com o seu gosto."/>
             </section>
 
             <section className="twocard">
-                <ApresentacaoCard icon="fa-solid fa-cart-shopping" msg="Compre seus pratos favoritos de maneira fácil e rápida com nossa interface intuitiva e fluida, proporcionando uma jornada de compra sem complicações."/>
+                <ApresentacaoCard icon="fa-solid fa-cart-shopping" msg="Peça suas refeições favoritas de forma simples e rápida, com uma interface intuitiva que facilita sua escolha."/>
 
-                <ApresentacaoCard icon="fa-solid fa-heart" msg="Favoritos seus restaurantes e refeições mais amadas, e compartilhe com sua rede de amigos para que eles também possam experimentar as melhores opções."/>
+                <ApresentacaoCard icon="fa-solid fa-heart" msg="Guarde seus pratos e restaurantes preferidos e compartilhe essas delícias com seus amigos."/>
             </section>
         </div>
     )
--- src/components/ApresentacaoCard/ApresentacaoCard.jsx
+++ src/components/ApresentacaoCard/ApresentacaoCard.jsx
@@ -1,8 +1,10 @@
 import "./ApresentacaoCard.css"
 
 export function ApresentacaoCard(props) {
-    <div className="cartao">
-        <div className="icon"><i class={props.icon}></i></div>
-        <p className="description">{props.msg}</p>
-    </div>
+    return(
+        <div className="cartao">
+            <div className="icon"><i class={props.icon}></i></div>
+            <p className="description">{props.msg}</p>
+        </div>
+    )
 }
