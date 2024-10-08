646fe4c8c8e0d11e77f626b3a67d18e32a4cca79
Leandro Souza <leandro2005souza@gmail.com>
2024-10-08 18:19:08 -0300

    passando css para styled component

--- src/components/CardsApp/CardsApp.jsx
+++ src/components/CardsApp/CardsApp.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './style.css'
+import styled from 'styled-components';
 
 export function CardsApp() {
     const [comidas, setComidas] = useState([]);
@@ -12,14 +13,63 @@ export function CardsApp() {
         fetchData();
     }, []);
 
+const CardsApp = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 360px;
+    overflow-x: hidden;
+    gap: 10px;
+    margin: 10px 0;
+
+    &::-webkit-scrollbar-track {
+        background-color: #fff4e1;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background-color: #E16D02;
+        border-radius: 5px;
+    }
+
+    &::-webkit-scrollbar {
+        width: 5px;
+    }
+`
+
+const CardComida = styled.div`
+    display: flex;
+    height: 50px;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 10px;
+    background-color: #fd8d0e6b;
+    padding: 10px 20px 10px 5px;
+    border-radius: 8px;
+    transition: all .3s ease;
+
+    &:hover {
+        background-color: #fd8d0e2a;
+        box-shadow: inset 0 0 0 2px #E16D02;
+    }
+
+    img {
+        height: 90%;
+    }
+
+    p {
+        padding: 0;
+        margin: 0;
+        font-weight: 600;
+    }
+`
+
     return (
-        <div className="cards-app">
+        <CardsApp>
             {comidas.map(comida => (
-                <div className='card-comida'>
+                <CardComida>
                     <img src={comida.strCategoryThumb} alt={comida.strCategory} />
-                    <p className="title">{comida.strCategory}</p>
-                </div>
+                    <p>{comida.strCategory}</p>
+                </CardComida>
             ))}
-        </div>
+        </CardsApp>
     )
 }
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,19 +1,59 @@
+import styled from "styled-components";
 import { Header } from "../Header/Header";
 import { MobileApp } from "../MobileApp/MobileApp";
-import './HeroSection.css'
+
+const HeroContent = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20px;
+    background-color: #FD8F0E;
+
+    @media screen and (max-width: 680px) {
+        flex-direction: column;
+        gap: 20px;
+    }
+`
+
+const HeroTexts = styled.div`
+    color: #fff4e1;
+    width: 40%;
+    text-align: start;
+
+    button {
+        border: #e16e02 solid 3px;
+        background-color: transparent;
+        color: #fff4e1;
+        padding: 15px 18px;
+        border-radius: 15px;
+        font-weight: 600;
+        transition: all .3s ease;
+    }
+
+    button:hover {
+        background-color: #fff4e1de;
+        color: #e16e02;
+        scale: 1.05;
+    }
+    
+    @media screen and (max-width: 680px) {
+        text-align: center;
+        width: 80%;
+    }
+`
 
 export function HeroSection() {
     return (
         <div className="hero-section">
             <Header />
-            <div className="hero-content">
-                <div className="texts">
+            <HeroContent>
+                <HeroTexts>
                     <h1>O melhor do mundo gastronômico em um só app</h1>
                     <h3>Descubra diversos tipos de culinária, peça e receba em sua casa!</h3>
                     <button>Baixe agora</button>
-                </div>
+                </HeroTexts>
                 <MobileApp />
-            </div>
+            </HeroContent>
         </div>
     )
 }
