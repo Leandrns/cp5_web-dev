0b8b7c8313174613b9e26880d45d70635ed75633
Viniciuscont2 <vinicont39@gmail.com>
2024-10-08 16:23:02 -0300

    Form e footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
 import { HeroSection } from './components/HeroSection/HeroSection';
+import FormularioContato from './components/Form/Form';
+import React from 'react';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <HeroSection />
+      <FormularioContato />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import "./Footer.css";
+
+export function Footer() {
+  return (
+    <div className="main-footer">
+      <div className="footer-columns">
+        <div className="footer-links">
+          <h4>Contato</h4>
+          <p>Telefone</p>
+          <a href="#">+55 1194529021</a>
+          <p>Email</p>
+          <a href="#">GourmetON@food.com.br</a>
+        </div>
+        <div className="social-media">
+          <h4>Redes Sociais</h4>
+          <i className="fa-brands fa-instagram" />
+          <i className="fa-brands fa-facebook" />
+        </div>
+        <div className="termo-uso">
+          <h6><a href="#">Termos de uso</a></h6>
+        </div>
+      </div>
+      <div className="footer">
+        <h6>&Copyright CLIRV</h6>
+      </div>
+    </div>
+  );
+}
