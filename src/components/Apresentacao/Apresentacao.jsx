3fb1fda36b1020ad4f868719ca813feeae314683
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-10-08 19:45:29 -0300

    styled

--- src/components/Apresentacao/Apresentacao.jsx
+++ src/components/Apresentacao/Apresentacao.jsx
@@ -1,19 +1,47 @@
 import { ApresentacaoCard } from "../ApresentacaoCard/ApresentacaoCard.jsx";
+import styled from 'styled-components';
+
+const Funcionalidades = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background: url(../../images/tela_estrela.png);
+  min-height: 750px;
+  padding-bottom: 35px;
+
+  @media screen and (max-width: 720px) {
+    flex-direction: column;
+    background: url(../../images/tela_estrela_mobile.png);
+  }
+`;
+
+const TwoCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  gap: 38px;
+  padding: 35px 0 0 0;
+
+  @media screen and (min-width: 1925px) {
+    flex-direction: row;
+    gap: 150px;
+  }
+`;
 
 export function Apresentacao() {
     return (
-        <div className="Apresentacao">
-            <section className="twocard">
+        <Funcionalidades>
+            <TwoCard>
                 <ApresentacaoCard icon="fa-solid fa-utensils" msg="Aproveite pratos deliciosos e frescos, com ingredientes de alta qualidade para uma experiência única."/>
 
                 <ApresentacaoCard icon="fa-solid fa-store" msg="Explore restaurantes próximos e descubra novos sabores que combinam com o seu gosto."/>
-            </section>
+            </TwoCard>
 
-            <section className="twocard">
+            <TwoCard>
                 <ApresentacaoCard icon="fa-solid fa-cart-shopping" msg="Peça suas refeições favoritas de forma simples e rápida, com uma interface intuitiva que facilita sua escolha."/>
 
                 <ApresentacaoCard icon="fa-solid fa-heart" msg="Guarde seus pratos e restaurantes preferidos e compartilhe essas delícias com seus amigos."/>
-            </section>
-        </div>
+            </TwoCard>
+            </Funcionalidades>
     )
 }
--- src/components/ApresentacaoCard/ApresentacaoCard.jsx
+++ src/components/ApresentacaoCard/ApresentacaoCard.jsx
@@ -1,10 +1,48 @@
-import "./ApresentacaoCard.css"
+import styled from 'styled-components';
+
+const Icon = styled.div`
+  color: #fff4e1;
+  font-size: 50px;
+  transition: all 0.2s ease;
+
+  &:hover {
+    color: #FD8F0E;
+    transform: scale(1.15);
+  }
+`;
+
+const Description = styled.p`
+  margin: 0;
+  font-size: 18px;
+  padding: 0 20px;
+  font-family: "Fredoka";
+`;
+
+const Cartao = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 290px;
+  height: 290px;
+  border: solid #E16D02;
+  background-color: #E16D02;
+  border-radius: 100%;
+  transition: all 0.5s ease;
+
+  &:hover {
+    transform: scale(1.15);
+    box-shadow: 0 0 8px #000;
+  }
+`;
+
 
 export function ApresentacaoCard(props) {
-    return(
-        <div className="cartao">
-            <div className="icon"><i class={props.icon}></i></div>
-            <p className="description">{props.msg}</p>
-        </div>
-    )
+  return (
+    <Cartao>
+      <Icon>
+        <i className={props.icon}></i>
+      </Icon>
+      <Description>{props.msg}</Description>
+    </Cartao>
+  );
 }
