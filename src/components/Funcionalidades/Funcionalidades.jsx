d11a96760d2c89ac5e3c409caf80d1c9219f5017
caio <caio62858@gmail.com>
2024-10-09 16:54:38 -0300

    styled components

--- src/components/Funcionalidades/Funcionalidades.jsx
+++ src/components/Funcionalidades/Funcionalidades.jsx
@@ -1,12 +1,21 @@
-import './style.css';
 import CardPratos from "../CardPratos/CardPratos";
+import styled from 'styled-components';
+
+const FuncionalidadesContainer = styled.div`
+  background-color: #FD8F0E;
+  font-family: "Fredoka";
+  padding-top: 20px;
+  padding-bottom: 20px;
+
+`;
 
 function Funcionalidades() {
-    return (
-      <div className="Funcionalidades">       
-        <CardPratos />
-      </div>
-    );
-  }
+  return (
+    <FuncionalidadesContainer>
+      <CardPratos />
+    </FuncionalidadesContainer>
+  );
+}
+
   export default Funcionalidades;
   
