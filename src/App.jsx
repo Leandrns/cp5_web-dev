6e11fa550f13220272b115360a6d66799c981ebd
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-10-09 10:44:42 -0300

    l

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import './App.css';
 import { Apresentacao } from './components/Apresentacao/Apresentacao';
 import { Depoimentos } from './components/Depoimentos/Depoimentos';
 import { Footer } from './components/Footer/Footer';
-import FormularioContato from './components/Form/Form';
 import Funcionalidades from './components/Funcionalidades/Funcionalidades';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FormularioContato from './components/Form/Form';
-import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { HeroSection } from './components/HeroSection/HeroSection';
 
