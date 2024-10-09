0f250698636444c4d86f7a83dd0bd27a2b78c920
Viniciuscont2 <vinicont39@gmail.com>
2024-10-09 15:01:41 -0300

    styled-components ,Footer e formulario

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,138 @@
-import "./Footer.css";
+import styled from 'styled-components';
+
+const MainFooter = styled.div`
+  background-color: #3f3f3f;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  color: white;
+`;
+
+const FooterColumns = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+
+  > * {
+    flex-basis: 25%;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+
+    > * {
+      flex-basis: 100%;
+      margin-bottom: 20px;
+    }
+  }
+  
+  @media (max-width: 480px) {
+    > * {
+      margin-bottom: 10px;
+    }
+  }
+`;
+
+const FooterLinks = styled.div`
+  padding: 10px;
+  margin: 10px;
+
+  h4 {
+    margin-bottom: 10px;
+  }
+
+  p, a {
+    margin: 0; /* Remove margens para p */
+    color: white;
+    text-decoration: none;
+
+    &:hover {
+      color: #e16d02;
+    }
+  }
+`;
+
+const SocialMedia = styled.div`
+  flex: 1;
+  margin: 10px;
+
+  h4 {
+    margin-bottom: 10px;
+  }
+
+  i {
+    padding: 10px;
+    transition: 0.5s;
+
+    &:hover {
+      color: #e16d02;
+      transform: scale(1.5);
+    }
+  }
+`;
+
+const TermoUso = styled.div`
+  flex: 1;
+  margin: 10px;
+
+  h4 {
+    margin-bottom: 10px;
+  }
+
+  h6 {
+    margin: 0;
+
+    a {
+      color: white;
+      text-decoration: none;
+
+      &:hover {
+        color: #e16d02;
+      }
+    }
+  }
+`;
+
+const FooterText = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #e16d02;
+  height: 35px;
+  margin-top: 20px; /* Espaçamento acima do texto do footer */
+
+  h6 {
+    margin: 0;
+  }
+`;
 
 export function Footer() {
   return (
-    <div className="main-footer">
-      <div className="footer-columns">
-        <div className="footer-links">
+    <MainFooter>
+      <FooterColumns>
+        <FooterLinks>
           <h4>Contato</h4>
           <p>Telefone</p>
           <a href="#">+55 1194529021</a>
           <p>Email</p>
           <a href="#">GourmetON@food.com.br</a>
-        </div>
-        <div className="social-media">
+        </FooterLinks>
+        <SocialMedia>
           <h4>Redes Sociais</h4>
           <i className="fa-brands fa-instagram" />
           <i className="fa-brands fa-facebook" />
-        </div>
-        <div className="termo-uso">
-            <h4>Politicas</h4>
+        </SocialMedia>
+        <TermoUso>
+          <h4>Políticas</h4>
           <h6><a href="#">Termos de uso</a></h6>
           <h6><a href="#">Lei do Consumidor</a></h6>
-        </div>
-      </div>
-      <div className="footer">
+        </TermoUso>
+      </FooterColumns>
+      <FooterText>
         <h6>© Copyright CLIRV | GOURMETON</h6>
-      </div>
-    </div>
+      </FooterText>
+    </MainFooter>
   );
 }
